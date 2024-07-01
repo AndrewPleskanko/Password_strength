078c9da6436cf09986503b3b599238a3bd64eb27
AndrewPleskanko <pleskankoandrew@gmail.com>
2024-07-01 16:14:21 +0300

    change logic

--- src/app/password/password.component.ts
+++ src/app/password/password.component.ts
@@ -17,10 +17,10 @@ export class PasswordComponent {
   password: string = '';
 
   getPasswordStrength(): string {
-    if (this.password.length < 8 && this.password.length > 0) {
-      return 'easy';
-    } else if (this.password.length < 8) {
+    if (this.password.length === 0) {
       return 'empty';
+    } else if (this.password.length < 8) {
+      return 'easy';
     } else if (this.password.match(/[a-zA-Z]/) && this.password.match(/[0-9]/) && this.password.match(/[!@#$%^&*()_+[\]{};':"\\|,.<>/?-]/)) {
       return 'strong';
     } else {
